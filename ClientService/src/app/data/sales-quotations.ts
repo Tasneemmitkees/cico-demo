30252bddeaea082dd7ae05ab94b4742e0102a6ed
tasneemmitkees <tasneemmitkees1999@gmail.com>
2023-02-09 11:16:18 +0200

    delete files

--- ClientService/src/app/data/sales-quotations.ts
+++
@@ -1,7 +0,0 @@
-export interface IsalesQuotation {
-  map(arg0: ({ id, status, date, netAmount }: { id: any; status: any; date: any; netAmount: any; }) => { id: any; status: any; date: any; netAmount: any; }): IsalesQuotation[];
-  id:number;
-  status: string;
-  date: string;
-  netAmount: number;
-}
--- ClientService/src/app/data/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User{
-  _id:number;
-  username:string;
-  role:string;
-  status:string;
-}
