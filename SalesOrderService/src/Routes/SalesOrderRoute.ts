b56c4f1c19033d0410ff862ae0e081a865b8f61e
sarafares <sarafares1999@gmail.com>
2023-02-09 14:00:00 +0200

    CICO api

--- SalesOrderService/src/Routes/SalesOrderRoute.ts
+++ SalesOrderService/src/Routes/SalesOrderRoute.ts
@@ -4,11 +4,12 @@ import { SalesOrderController } from "../Controllers/SalesOrderController";
 const salesOrderController = new SalesOrderController()
 const salesOrderRouter = express.Router()
 
-salesOrderRouter.post('/SalesOrder',salesOrderController.addSalesOrder)
-salesOrderRouter.post('/SalesOrder/addWithRef/:quoteID/:buyerPartyID',salesOrderController.addSalesOrderWithRef)
+//salesOrderRouter.post('/SalesOrder',salesOrderController.addSalesOrder)
+salesOrderRouter.post('/SalesOrder',salesOrderController.addSalesOrderWithRef)
+salesOrderRouter.post('/token',SalesOrderController.get_Token)
 // salesOrderRouter.delete('/SalesOrder/:orderID',salesOrderController.deleteSalesOrder)
 // salesOrderRouter.put('/SalesOrder/:orderID',salesOrderController.updateSalesOrder)
-salesOrderRouter.get('/SalesOrder/:orderID',salesOrderController.getSalesOrder)
-salesOrderRouter.get('/SalesOrder',salesOrderController.getAllSalesOrder)
-
+// salesOrderRouter.get('/SalesOrder/:orderID',salesOrderController.getSalesOrder)
+// salesOrderRouter.get('/SalesOrder',salesOrderController.getAllSalesOrder)
+///SalesOrder/addWithRef/:quoteID/:buyerPartyID
 export{salesOrderRouter}
